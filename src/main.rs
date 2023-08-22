565566d830ecf0a31b9687dca3d779b75723ddd0
yisangGFX <qinchen224@gmail.com>
2023-08-22 15:11:08 +0800

    First commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+extern crate vulkano;
+extern crate winit;
+
+use winit::{EventsLoop, WindowBuilder, dpi::LogicalSize,  WindowEvent, Event};
+
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
+struct  HelloTriangleApplication {
+    events_loop: EventsLoop,
+}
+
+impl HelloTriangleApplication {
+    pub fn initialize() -> Self {
+        let events_loop = Self::init_window();
+
+        Self {
+            events_loop,
+        }
+    }
+
+    fn init_window() -> EventsLoop {
+        let events_loop = EventsLoop::new();
+        let _window = WindowBuilder::new()
+            .with_title("Vulkan")
+            .with_dimensions(LogicalSize::new(f64::from(WIDTH), f64::from(HEIGHT)))
+            .build(&events_loop).unwrap();
+        events_loop
+    }
+
+    fn main_loop(&mut self) {
+        loop {
+            let mut done = false;
+            self.events_loop.poll_events(|ev| {
+                if let Event::WindowEvent { event: WindowEvent::CloseRequested, .. } = ev {
+                    done = true
+                }
+            });
+            if done {
+                return;
+            }
+        }
+    }
+}
+fn main() {
+    let mut app = HelloTriangleApplication::initialize();
+    app.main_loop();
+}
